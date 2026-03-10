6d214e2c27657eea014717c3e190bdf7dc898c8b
Raginaakhter <raginaakhter052@gmail.com>
2026-03-10 23:59:05 +0600

    createede

--- src/context/Demo.jsx
+++ src/context/Demo.jsx
@@ -4,7 +4,7 @@ const Demo = () => {
     return (
         <div>
            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem rem perspiciatis itaque ut placeat quibusdam sapiente asperiores magni, voluptate architecto doloremque
-            velit dignissimos rerum explicabo nulla natus, corrupti harum reiciendis.
+            velit dignissimos rerum explicabo nulla 
         </div>
     );
 };
